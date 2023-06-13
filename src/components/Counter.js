d6453a0d1192852824e9d5ae85478232fdbab0a3
DivyaKanchugatla <divyakanchugatla5@gmail.com>
2023-06-13 13:17:28 +0530

    updated

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React ,{useState} from "react";
 import { useDispatch,  useSelector } from "react-redux"
 
 const Counter = () => {
     const count = useSelector(state=>state.count)
+    const [randomNum,setRandomNum] = useState("")
     const dispatch = useDispatch()
     const handleDecrement = () =>{
         dispatch({
-            type:"DECREMENT"
+            type:"DECREMENT",
+            payload:Number(randomNum)
          })
+
+        
     }
     const handleReset = () =>{
         dispatch({
             type:"RESET"
+           
          })
     }
     const handleIncrement = () =>{
         dispatch({
-            type:"INCREMENT"
+            type:"INCREMENT",
+            payload:Number(randomNum)
          })
     }
     return (
         <>
         <div className="counter-container">
             <h1>Counter Application</h1>
+            <label>
+            Number to Add :     
+            <input type="number" value={randomNum} onChange={(e)=>setRandomNum(e.target.value)} placeholder="Enter any number" style={{margin:"10px"}} />
+            </label>
             <p><span>Count </span>: {count}</p>
             <div className="buttons-container">
                 <button onClick={handleDecrement}>Decrement</button>
